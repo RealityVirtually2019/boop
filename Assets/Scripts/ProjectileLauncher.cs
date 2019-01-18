0217e44b1058fa465d1b18fd18f4085e6b24b3be
Sagar Ramesh <sagarramesh97@gmail.com>
2019-01-18 14:37:17 -0500

    Now able to switch between different projectiles

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -6,7 +6,8 @@ public class ProjectileLauncher : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject projectile;
+    private GameObject p1, p2, p3;
+    int selectedProjectile = 1;
     [SerializeField]
     private float forceAmount;
     [SerializeField]
@@ -26,13 +27,39 @@ public class ProjectileLauncher : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             LaunchObject();
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            selectedProjectile = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)){
+            selectedProjectile = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)){
+            selectedProjectile = 3;
+        }
     }
 
     private void LaunchObject()
     {
         // Spawn projectile and play corresponding sound:
         GameObject spawned;
-        spawned = Instantiate(projectile);
+        if (selectedProjectile == 1)
+        {
+            spawned = Instantiate(p1);
+        }
+        else if (selectedProjectile == 2)
+        {
+            spawned = Instantiate(p2);
+        }
+        else if (selectedProjectile == 3)
+        {
+            spawned = Instantiate(p3);
+        }
+        else
+        {
+            spawned = Instantiate(p1);
+        }
+
         GetComponent<AudioSource>().PlayOneShot(launchSound);
 
         // Launch projectile from source:
