5675dcffc9fdc11b913f642ba958bfd4633ec938
Sagar Ramesh <sagarramesh97@gmail.com>
2019-01-19 23:53:20 -0500

    Committing new changes

--- Boop_ML/Assets/Scripts/Controller.cs
+++ Boop_ML/Assets/Scripts/Controller.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,14 @@ public class Controller : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         MLInput.Start();
+
         _controller = MLInput.GetController(MLInput.Hand.Left);
 	}
 
     void OnDestroy()
     {
         MLInput.Stop();
+
     }
 
     // Update is called once per frame
@@ -22,3 +25,31 @@ public class Controller : MonoBehaviour {
         transform.position = _controller.Position;
 	}
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Controller : MonoBehaviour {
+
+    MLInputController _controller;
+	// Use this for initialization
+	void Start () {
+        MLInput.Start();
+
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+	}
+
+    void OnDestroy()
+    {
+        MLInput.Stop();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = _controller.Position;
+	}
+}
+>>>>>>> 9b189e3074a19b8dd7ccfbbf9fc2ef617e555f62
