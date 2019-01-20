e242bc101e484f7478eb2b0005ba88d93b41f7ef
Sagar Ramesh <sagarramesh97@gmail.com>
2019-01-19 23:51:01 -0500

    Egg and Sagar's Cannon scene

--- Boop_ML/Assets/Scripts/ButtonPressed.cs
+++ Boop_ML/Assets/Scripts/ButtonPressed.cs
@@ -25,5 +25,19 @@ public class ButtonPressed : MonoBehaviour {
             DC.actionType = 3;
             PL.LaunchObject();
         }
+        else if (this.name == "4")
+        {
+            // DROP EGG
+        }
+        else if (this.name == "5")
+        {
+            // Baby face:
+            DC.faceType = 1;
+        }
+        else if (this.name == "6")
+        {
+            // Chicken face:
+            DC.faceType = 2;
+        }
     }
 }
